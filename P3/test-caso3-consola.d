7e64d774b1ba4f4d22c698534513071a42df3e4d
Lokeon <kevin.lopezcala99@gmail.com>
2018-05-15 13:18:14 +0200

    168/168

--- P3/test-caso3-consola.d
+++
@@ -1,3 +0,0 @@
-test-caso3-consola.o test-caso3-consola.d : test-caso3-consola.cpp usuario.hpp ../P1/cadena.hpp \
-  articulo.hpp ../P1/fecha.hpp tarjeta.hpp pedido.hpp usuario-pedido.hpp \
-  pedido-articulo.hpp
