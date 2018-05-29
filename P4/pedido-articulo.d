0c65289e959c1709138453a3b35fe2a70783f6a2
Lokeon <kevin.lopezcala99@gmail.com>
2018-05-29 17:31:34 +0200

    update

--- P4/pedido-articulo.d
+++
@@ -1,2 +0,0 @@
-pedido-articulo.o pedido-articulo.d : pedido-articulo.cpp pedido-articulo.hpp pedido.hpp \
-  tarjeta.hpp ../P1/cadena.hpp ../P1/fecha.hpp usuario.hpp articulo.hpp
--- P4/test-caso4-consola.d
+++
@@ -1,3 +0,0 @@
-test-caso4-consola.o test-caso4-consola.d : test-caso4-consola.cpp usuario.hpp ../P1/cadena.hpp \
-  articulo.hpp ../P1/fecha.hpp tarjeta.hpp pedido.hpp usuario-pedido.hpp \
-  pedido-articulo.hpp
